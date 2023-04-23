8c147fc3ff5b598cffbab085999685cc47e19397
Kristan Ruíz <itan_07@hotmail.com>
2023-04-23 10:04:45 -0800

    Borrando los otros  metodos que nunca funcionaron

--- test.cs
+++ test.cs
@@ -39,48 +39,7 @@ namespace probabildiadCalculadorCarros
             {
                 Console.WriteLine("No se encontró el valor");
             }
-
-
-            //// Hacer la petición HTTP a la página
-            //WebClient client = new WebClient();
-            //string html = client.DownloadString("https://autos.mercadolibre.com.mx/march/#filter");
-
-            //// Analizar el HTML con HtmlAgilityPack
-            //HtmlDocument doc = new HtmlDocument();
-            //doc.LoadHtml(html);
-
-            //// Encontrar la línea que tiene el valor 'dimension22'
-            //HtmlNode node = doc.DocumentNode.SelectSingleNode("//script[contains(text(),'dimension22')]");
-
-            //// Extraer el número de la línea usando regular expressions
-            //string value = Regex.Match(node.InnerText, @"\b\d+\b").Value;
-
-            //Console.WriteLine("Valor: " + value);
-
-
-
-
-
-
         }
-
-
-
-        //// Hacer la petición HTTP a la página
-        //WebClient client = new WebClient();
-        //string html = client.DownloadString("https://autos.mercadolibre.com.mx/march/#filter");
-
-        //// Analizar el HTML con HtmlAgilityPack
-        //HtmlDocument doc = new HtmlDocument();
-        //doc.LoadHtml(html);
-
-        //// Encontrar el número de publicaciones
-        //HtmlNode node = doc.DocumentNode.SelectSingleNode("//span[@class='quantity-results']");
-        //string result = node.InnerText.Trim().Replace(",", "");
-
-        //int numPublicaciones = Int32.Parse(result);
-
-        //Console.WriteLine("Número de publicaciones: " + numPublicaciones);
     }
 }
 
